8b90c1291c731de31c3eda67eb266fca2fa9035d
vagrant <vagrant@codeup-trusty>
2014-06-02 15:25:43 +0000

    added alternative syntax

--- public/todo_list.php
+++ public/todo_list.php
@@ -3,7 +3,7 @@ define('FILENAME', 'data/list.txt');
 
 $to_do_items = [];
 
-var_dump($_GET);
+// var_dump($_GET);
 
 
 function read_file($filename) {
@@ -35,8 +35,30 @@ if (isset($_POST['TODO_item'])) {
 	write_file(FILENAME, $to_do_items);
 }
 
+// var_dump($_FILES);
 
+if (count($_FILES) > 0 && $_FILES['file1']['error'] == 0)
+{
+	if ($_FILES['file1']['type'] == 'text/plain') {
+	    $upload_dir = '/vagrant/sites/todo.dev/public/uploads/';
+	    $filename1 = basename($_FILES['file1']['name']);
+	    $saved_filename = $upload_dir . $filename1;
+	    move_uploaded_file($_FILES['file1']['tmp_name'], $saved_filename);
+	} else {
+		echo "This is not a text file, please try again!";
+	}
+}
+
+// Check if we saved a file
+if (isset($saved_filename)) {
+    $file_todo = $saved_filename;
+    $newfile = read_file($file_todo);
+    // var_dump($newfile);
+    $to_do_items = array_merge($to_do_items, $newfile);
+    write_file(FILENAME, $to_do_items);
+}
 ?>
+
 <!DOCTYPE html>
 <html>
 	<head>
@@ -45,11 +67,9 @@ if (isset($_POST['TODO_item'])) {
 	<body>
 		<h2>TODO List</h2>
 <ul>		
-		<?php		
-			foreach($to_do_items as $key => $item) {
-				echo "<li>$item<a href = '?id=$key'>Mark Complete</a></li>";
-			} 	
-		?>
+		<?	foreach($to_do_items as $key => $item): ?>
+				 <li><?= "$item <a href = '?id=$key'>Mark Complete</a>"; ?></li> 	
+		<?endforeach;?>
 </ul>		
 		<p>Please enter any additional TODO items on the form below. Please click submit when you have completed entering your items.</p>
 		<form method="POST">
@@ -61,5 +81,16 @@ if (isset($_POST['TODO_item'])) {
 			    <button type="submit">Submit</button>
 			</p>
 		</form>
+		<h1>Upload File</h1>
+
+	<form method="POST" enctype="multipart/form-data">
+	    <p>
+	        <label for="file1">File to upload: </label>
+	        <input type="file" id="file1" name="file1">
+	    </p>
+	    <p>
+	        <input type="submit" value="Upload">
+	    </p>
+	</form>	
 	</body>
 </html>
